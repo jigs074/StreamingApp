e7f3a4bb167b3c2feb273e2d6c201f266987f1e3
jigs074 <90673637+jigs074@users.noreply.github.com>
2024-08-08 14:39:56 -0400

    Peer Js server is now switched to the public server and it is tested now the video call functionality is working on other devices as well

--- public/script.js
+++ public/script.js
@@ -71,9 +71,11 @@
 const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
-    host: '/', 
-    port: '3001'
+    host: '0.peerjs.com', 
+    port: '443'
 });
+
+
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 const peers = {};
